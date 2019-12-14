730f9e30cca67ea3bf2f78a21826afab0a19dc15
Ruslan <h.ruslan97@gmail.com>
2019-12-14 19:24:54 +0600

    Added permission for Photo

--- source/api/views.py
+++ source/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.serializer import CommentSerializer, LikeSerializer
@@ -15,6 +17,9 @@ class CommentViewSet(ModelViewSet):
             return [AllowAny()]
         return [IsAuthenticated()]
 
+    def perform_create(self, serializer):
+        serializer.save(author= self.request.user)
+
 
 class LikeViewSet(ModelViewSet):
     queryset = Like.objects.all()
@@ -24,3 +29,19 @@ class LikeViewSet(ModelViewSet):
         if self.action in SAFE_METHODS:
             return [AllowAny()]
         return [IsAuthenticated()]
+
+    @action(methods=['post'], detail=True)
+    def like(self, request, pk=None):
+        like = self.get_object()
+        if like.like != True:
+            like.like = True
+            like.save()
+            return Response({'id': like.photo.pk, 'like': like.like})
+
+    @action(methods=['post'], detail=True)
+    def like(self, request, pk=None):
+        like = self.get_object()
+        if like.like == True:
+            like.like = False
+            like.save()
+            return Response({'id': like.photo.pk, 'like': like.like})
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin, PermissionRequiredMixin, LoginRequiredMixin
 
 from webapp.forms import PhotoForm
 from webapp.models import Photo
@@ -21,10 +22,11 @@ class PhotoView(DetailView):
     context_object_name = 'photo'
 
 
-class PhotoCreate(CreateView):
+class PhotoCreate(LoginRequiredMixin, CreateView):
     model = Photo
     template_name = 'create.html'
     form_class = PhotoForm
+    permission_denied_message = 'Доступ запрещен'
 
     def get_success_url(self):
         return reverse('webapp:photo', kwargs={'pk': self.object.pk})
@@ -36,20 +38,39 @@ class PhotoCreate(CreateView):
         return HttpResponseRedirect(self.get_success_url())
 
 
-class PhotoUpdate(UpdateView):
+class PhotoUpdate(UserPassesTestMixin, UpdateView):
     form_class = PhotoForm
     template_name = 'update.html'
     model = Photo
     context_object_name = 'photo'
+    # permission_required = 'webapp.change_photo'
+    # permission_denied_message = 'Запрещено'
+
+    # def test_func(self):
+    #     photo = self.get_object()
+    #     if self.request.user == photo.author or self.request.user.is_superuser \
+    #            or self.request.user.has_perm('webapp.change_photo'):
+    #         return True
+    #     return False
+
+    def test_func(self):
+        photo = Photo.objects.filter(author__username=self.request.user, pk=self.kwargs['pk'])
+        if photo or self.request.user.has_perm('webapp.change_photo'):
+            return True
 
     def get_success_url(self):
         return reverse('webapp:photo', kwargs={'pk': self.object.pk})
 
 
-class PhotoDelete(DeleteView):
+class PhotoDelete(UserPassesTestMixin, DeleteView):
     template_name = 'delete.html'
     model = Photo
     context_object_name = 'photo'
+
+    def test_func(self):
+        photo = Photo.objects.filter(author__username=self.request.user, pk=self.kwargs['pk'])
+        if photo or self.request.user.has_perm('webapp.delete_photo'):
+            return True
 
     def get_success_url(self):
         return reverse_lazy('webapp:index')
